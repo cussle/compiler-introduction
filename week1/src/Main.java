f0e7979ea043cd80336a487768936190ef4fbde6
cussle <cussle@kakao.com>
2024-09-13 19:05:48 +0900

    feat: C 코드 시작/종료부 코드 구현

--- week1/src/Main.java
+++ week1/src/Main.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 // imolang 덧셈연산: `:\}\^{0,3}\^{0,3}`
@@ -8,9 +10,17 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new FileReader("./src/test.imlg"));
-        String temp = "";
-        while ((temp = input.readLine()) != null) {
-            System.out.println(temp);
-        }
+        BufferedWriter output = new BufferedWriter(new FileWriter("./src/test.c"));
+
+        // C 코드의 시작부
+        output.write("#include <stdio.h>\n");
+        output.write("int main() {\n");
+
+        // C 코드의 종료부
+        output.write("\treturn 0;\n");
+        output.write("}\n");
+
+        input.close();
+        output.close();
     }
 }
