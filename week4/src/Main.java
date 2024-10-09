99ded72c3a25bbf49c529a7d5691028a183e9510
cussle <cussle@kakao.com>
2024-10-09 15:55:03 +0900

    feat: terminal symbol에 대한 procedure 구현

--- week4/src/Main.java
+++ week4/src/Main.java
@@ -6,6 +6,14 @@ public class Main {
     private static int index;  // 현재 문자열의 index를 관리하는 전역변수
     private static String input;  // 입력받은 문자열을 담는 전역변수
 
+    // Terminal Symbol
+    private static final char TOKEN_a = 'a';
+    private static final char TOKEN_b = 'b';
+    private static final char TOKEN_c = 'c';
+    private static final char TOKEN_d = 'd';
+    private static final char TOKEN_e = 'e';
+    private static final char TOKEN_f = 'f';
+
     // 문자열의 끝에 도달했는지 확인하는 메서드
     private static boolean isEnd() {
         return index >= input.length();
@@ -24,6 +32,60 @@ public class Main {
         index++;
     }
 
+    // Terminal Symbol `a`에 대한 procedure
+    private static boolean pa() {
+        if (currentSymbol() == TOKEN_a) {
+            nextSymbol();
+            return true;
+        }
+        return false;
+    }
+
+    // Terminal Symbol `b`에 대한 procedure
+    private static boolean pb() {
+        if (currentSymbol() == TOKEN_b) {
+            nextSymbol();
+            return true;
+        }
+        return false;
+    }
+
+    // Terminal Symbol `c`에 대한 procedure
+    private static boolean pc() {
+        if (currentSymbol() == TOKEN_c) {
+            nextSymbol();
+            return true;
+        }
+        return false;
+    }
+
+    // Terminal Symbol `d`에 대한 procedure
+    private static boolean pd() {
+        if (currentSymbol() == TOKEN_d) {
+            nextSymbol();
+            return true;
+        }
+        return false;
+    }
+
+    // Terminal Symbol `e`에 대한 procedure
+    private static boolean pe() {
+        if (currentSymbol() == TOKEN_e) {
+            nextSymbol();
+            return true;
+        }
+        return false;
+    }
+
+    // Terminal Symbol `f`에 대한 procedure
+    private static boolean pf() {
+        if (currentSymbol() == TOKEN_f) {
+            nextSymbol();
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -34,9 +96,9 @@ public class Main {
         sb.append("\t").append("B -> d | e | f").append("\n\n");
 
         sb.append("input: ");
+        System.out.print(sb.toString());
+
         input = br.readLine();
         br.close();
-        
-        System.out.println(sb.toString());
     }
 }
