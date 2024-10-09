84758e29fb94ba50405c55ea31d722f54064b24f
cussle <cussle@kakao.com>
2024-10-09 15:25:34 +0900

    feat: CFG 출력 추가

--- week4/src/Main.java
+++ week4/src/Main.java
@@ -1,6 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("CFG:").append("\n");
+        sb.append("\t").append("S -> aA | bB").append("\n");
+        sb.append("\t").append("A -> aBb | bBb | cBb").append("\n");
+        sb.append("\t").append("B -> d | e | f").append("\n\n");
+
+        sb.append("input: ");
+
+        System.out.println(sb.toString());
     }
 }
