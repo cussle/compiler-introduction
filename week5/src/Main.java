06692bd96c6d371785c92bcb492f338dc2d4e0ae
cussle <cussle@kakao.com>
2024-10-17 15:45:51 +0900

    feat: 기존 코드에 주석 추가 및 StringBuilder 적용

--- week5/src/Main.java
+++ week5/src/Main.java
@@ -10,16 +10,20 @@ import org.antlr.v4.runtime.tree.ParseTreeWalker;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        // 파일에서 입력 스트림을 읽고, lexer를 통해 토큰화
         CharStream code = CharStreams.fromFileName("./src/test.tr");
         tinyR4Lexer lexer = new tinyR4Lexer(code);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
 
+        // 파서를 통해 프로그램을 파싱한 후 파스트리 생성
         tinyR4Parser parser = new tinyR4Parser(tokens);
         ParseTree tree = parser.program();
 
+        // walker를 사용해 파스트리를 순회하며 listener가 이벤트를 처리하도록 함
         ParseTreeWalker walker = new ParseTreeWalker();
         walker.walk(new tinyR4PrintListener(), tree);
 
+        // 파싱 결과 출력
         System.out.println(tinyR4PrintListener.getOutput());
     }
 }
--- week5/src/tinyR4PrintListener.java
+++ week5/src/tinyR4PrintListener.java
@@ -5,19 +5,22 @@ import org.antlr.v4.runtime.tree.ParseTreeProperty;
 
 public class tinyR4PrintListener extends tinyR4BaseListener implements ParseTreeListener {
 
-    private static String output;
-    ParseTreeProperty<String> r4Tree = new ParseTreeProperty<>();
+    private static String output;  // 프로그램의 출력 결과
+    ParseTreeProperty<String> r4Tree = new ParseTreeProperty<>();  // 파스트리의 각각의 노드 값을 저장
 
+    // 파싱 결과를 얻기 위한 static 메서드
     public static String getOutput() {
         return output;
     }
 
+    // 프로그램 전체를 방문한 후 decl을 처리하여 output을 설정
     @Override
     public void exitProgram(tinyR4Parser.ProgramContext ctx) {
-        String program = "";
+        StringBuilder program = new StringBuilder();
         for (int i = 0; i < ctx.decl().size(); i++) {
-            program += r4Tree.get(ctx.decl(i));
+            // 각 decl의 출력을 가져와 문자열에 추가
+            program.append(r4Tree.get(ctx.decl(i)));
         }
-        output = program;
+        output = program.toString();
     }
 }
